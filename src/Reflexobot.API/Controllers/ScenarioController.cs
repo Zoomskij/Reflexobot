41f52dc5d35e048d974159db6a5856e58c5cb22c
potapov artem <zoomskij@gmail.com>
2022-06-25 20:41:16 +0300

    scenarios not finished

---
+++ src/Reflexobot.API/Controllers/ScenarioController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Reflexobot.Models;
+using Reflexobot.Services.Interfaces;
+
+namespace Reflexobot.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ScenarioController : Controller
+    {
+        private readonly IScenarioService _scenarioService;
+        public ScenarioController(IScenarioService scenarioService)
+        {
+            _scenarioService = scenarioService;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult GetAchievments()
+        {
+            var scenarios = _scenarioService.Get().OrderBy(x => x.CreatedDate);
+            
+            List<ScenarioDto> scenarioDtoList = new List<ScenarioDto>();
+
+            foreach (var scenario in scenarios)
+            {
+                scenarioDtoList.Add(new ScenarioDto
+                {
+                    Id = scenario.Guid,
+                    ParrentGuid = scenario.ParrentGuid,
+                    CreatedDate = scenario.CreatedDate,
+                    Command = scenario.Command,
+                    Label = !string.IsNullOrWhiteSpace(scenario.Text) ? scenario.Text : scenario.Command,
+                    Children = new List<ScenarioDto>()
+                });
+            }
+
+            var groups = scenarioDtoList.GroupBy(x => x.ParrentGuid);
+            List<ScenarioDto> scenarioDto = new List<ScenarioDto>();
+
+            foreach (var group in groups)
+            {
+                foreach (var item in group)
+                {
+                    var b = group.Select(x => x);
+                    scenarioDto.Add(new ScenarioDto
+                    {
+                        Id = item.Id,
+                        ParrentGuid = item.ParrentGuid,
+                        CreatedDate = item.CreatedDate,
+                        Command = item.Command,
+                        Label = item.Label,
+                        Children = group.Select(x => x).ToList()
+                    });
+                }
+            }
+
+            return Ok(scenarioDto);
+        }
+
+    }
+}
---
+++ src/Reflexobot.Models/ScenarioDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflexobot.Models
+{
+    public class ScenarioDto
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public Guid? ParrentGuid { get; set; } = null;
+        public string Command { get; set; }
+        public string Label { get; set; }
+        public List<ScenarioDto> Children { get; set; }
+    }
+}
